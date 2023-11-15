ad1ce10b976f6213e1bbc48650a80313b4bc91df
bobbyup <57191881+bobbyup@users.noreply.github.com>
2023-11-15 15:00:46 -0500

    updated limit_economy to use broadcasting instead of replacement

--- limit_economy.jl
+++ limit_economy.jl
@@ -1,21 +1,35 @@
 function limit_economy(stu_types, μ_i, sch_types, μ_s, grid_copies, num_sim)
+
+    # Initialize the set of cutoff values
     cutoff_vals = zeros(Float64, length(grid_copies),length(μ_s)+1)
+    new_stat = zeros(Float64,length(μ_s)+1)
+
+    # For each of the grid values we want to calculate
     i = 1
     for num_copies in grid_copies
-        new_stat = zeros(Float64,length(μ_s)+1)
+        new_stat .= zeros(Float64,length(μ_s)+1)
+
+        # If we run multiple simulations for one value, we repeat the economy here
         for i in 1:num_sim 
+
+            # Expand economy 
             students = expand_stu_pref(stu_types, μ_i, num_copies)
             sch_priority, tie_breaker = expand_priority(sch_types, students, μ_s)
             capacity = expand_capacity(μ_s, num_copies)
+
+            # Run expanded economy
             assn, ranks = deferredacceptance(students, sch_priority, capacity)
+
+            #Analyze statistics
             assn_stats = gen_sch_stats(assn, capacity, sch_priority, students, tie_breaker)
-            new_stat = new_stat + assn_stats.cutoffs
+            new_stat .= new_stat .+ assn_stats.cutoffs
         end
         display(new_stat./ num_sim)
         cutoff_vals[i,:] = new_stat ./num_sim 
         i = i + 1
     end
     plot(grid_copies, cutoff_vals)  
-    CSV.write("cutoffs.csv",  Tables.table(cutoff_vals), writeheader=false)
     savefig("myplot.png")
+
+    CSV.write("cutoffs.csv",  Tables.table(cutoff_vals), writeheader=false)
 end
--- master.jl
+++ master.jl
@@ -6,34 +6,10 @@ include("big_da.jl")
 include("analysis.jl")
 include("limit_economy.jl")
 
-stu_types, μ_i, sch_types, μ_s = load_files("3sch_sd_exp")
+stu_types, μ_i, sch_types, μ_s = load_files("base_exp")
 
 grid_copies = [100*i for i in 1:25]
 limit_economy(stu_types, μ_i, sch_types, μ_s, grid_copies, 10)
-stophere
-
-# Expand student preferences
-num_copies = 1000000
-students = expand_stu_pref(stu_types, μ_i, num_copies)
-display(students)
-CSV.write("test_stu.csv",  Tables.table(students), writeheader=false)
-# Generate school priorities
-sch_priority, tie_breaker = expand_priority(sch_types, students, μ_s)
-display(sch_priority)
-CSV.write("test_pri.csv",  Tables.table(sch_priority), writeheader=false)
-CSV.write("test_tie_breaker.csv",  Tables.table(sch_priority), writeheader=false)
-
-# Capacity expansion
-capacity = expand_capacity(μ_s, num_copies)
-display(capacity)
-CSV.write("test_cap.csv",  Tables.table(capacity), writeheader=false)
-
-assn, ranks = deferredacceptance(students, sch_priority, capacity)
-display(assn)
-CSV.write("test_assn.csv",  Tables.table(assn), writeheader=false)
-
-display(tie_breaker)
-gen_sch_stats(assn, capacity, sch_priority, students, tie_breaker)
 
 
 
